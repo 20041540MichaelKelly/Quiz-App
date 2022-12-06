c6899ea2b8b3d6c4e71ed0ed87d4e33e152ab0c0
Michael Kelly <mikelly1988@gmail.com>
2022-12-06 00:12:27 +0000

    started implementing the anwser process

--- app/src/main/java/mick/projects/quizapp/QuestionsActivity.kt
+++ app/src/main/java/mick/projects/quizapp/QuestionsActivity.kt
@@ -1,15 +1,25 @@
 package mick.projects.quizapp
 
+import android.app.PendingIntent.OnFinished
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedQuestion: Int = 0
+
+    private var mSubmitButton: Button? = null
+
     private var progressBar: ProgressBar?= null
     private var tvProgressBar: TextView?= null
     private var imageView: ImageView?= null
@@ -31,22 +41,52 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
         answerTwo = findViewById(R.id.anwserTwo)
         answerThree = findViewById(R.id.anwserThree)
         answerFour = findViewById(R.id.anwserFour)
+        mQuestionList = Constants.getQuestions()
 
-        var questionList = Constants.getQuestions()
+        setQuestions()
+        defaultOptionsView()
+    }
 
-        for(i in questionList) {
-            Log.e("Question", i.question)
-        }
-
-        var currentPosition = 1
-        val question: Question = questionList[currentPosition -1]
-        progressBar?.progress = currentPosition
-        tvProgressBar?.text = "$currentPosition/ ${progressBar?.max}"
+    private fun setQuestions() {
+        val question: Question = mQuestionList!![mCurrentPosition -1]
+        progressBar?.progress = mCurrentPosition
+        tvProgressBar?.text = "$mCurrentPosition/ ${progressBar?.max}"
         tvQuestion?.text = question.question
         answerOne?.text = question.questionOne
         answerTwo?.text = question.questionTwo
         answerThree?.text = question.questionThree
         answerFour?.text = question.questionFour
+
+        if(mCurrentPosition == mQuestionList!!.size) {
+            mSubmitButton?.text = "FINISH"
+        } else {
+            mSubmitButton?.text = "SUBMIT"
+        }
+
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+
+        answerOne?.let {
+            options.add(0, it)
+        }
+        answerTwo?.let {
+            options.add(1, it)
+        }
+        answerThree?.let {
+            options.add(2, it)
+        }
+        answerFour?.let {
+            options.add(3, it)
+        }
+
+        for(option in options) {
+            option.setTextColor(Color.parseColor("#476234f"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_background)
+        }
+
     }
 
     override fun onClick(p0: View?) {
