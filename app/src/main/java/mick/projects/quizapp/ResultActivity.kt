41a14d3daee18a82bbf3ece1110cd70367ec7e4c
Michael Kelly <mikelly1988@gmail.com>
2023-01-07 20:46:25 +0000

    Updated with some new questions and response based on the amount of anwsers correct

--- app/src/main/java/mick/projects/quizapp/ResultActivity.kt
+++ app/src/main/java/mick/projects/quizapp/ResultActivity.kt
@@ -12,6 +12,7 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result)
 
         val tvName: TextView = findViewById(R.id.tv_name)
+        val tvCongrats: TextView = findViewById(R.id.tv_congratulations)
         val tvScore: TextView = findViewById(R.id.tv_score)
         val btnFinish: Button = findViewById(R.id.btn_finish)
 
@@ -19,6 +20,14 @@ class ResultActivity : AppCompatActivity() {
 
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+
+        if(correctAnswers <= 3) {
+            tvCongrats.text = getString(R.string.less_than_or_equal_three)
+        }else if( correctAnswers <= 7 ) {
+            tvCongrats.text = getString(R.string.less_than_or_equal_seven)
+        } else {
+            tvCongrats.text = getString(R.string.victory)
+        }
 
         tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
 
